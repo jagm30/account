3c5bac4ff090af8c7aeff21061b388673d06ec97
jagm30 <jose.antoniosiiayuda@gmail.com>
2023-06-16 22:49:36 -0600

    correcion clientes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/usuarios';
+    public const HOME = '/home';
 
     /**
      * The controller namespace for the application.
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
-@section('content')
+@section('contenidoprincipal')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Bienvenido') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    Sistema de Facturación 1.0
                 </div>
             </div>
         </div>
